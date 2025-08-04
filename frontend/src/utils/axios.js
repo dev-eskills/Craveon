b54ace42e13d057ee0b94f98e202e87fa9c9fbaa
dev-eskills <developmenteskills@gmail.com>
2025-08-04 17:57:28 +0530

    Update axios.js

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -3,8 +3,8 @@ import { useAuthStore } from '../stores/authStore';
 
 const api = axios.create({
 
-  baseURL: 'https://craveonapi.onrender.com/api', // for production
-
+  // baseURL: 'https://craveonapi.onrender.com/api', // for production
+  baseURL:"https://craveon-backend.onrender.com/api"  // eskills development
   // baseURL: 'http://localhost:3000/api', // for development
   withCredentials: true,
 });
